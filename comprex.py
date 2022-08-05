727fa7e7b2d23303fd9d734000f9c6668546e694
Pessi Vilppolahti <pessiv@outlook.com>
2022-08-05 17:11:15 +0300

    special sequences

--- comprex.py
+++ comprex.py
@@ -32,3 +32,22 @@ class ComprehensibleRegex:
         return "{}".format(a)
     def format_two(self,a,b) -> str:
         return "{}{}".format(a,b)
+    def special_sequences(self,n):
+        """
+        0 = Returns a match if the specified characters are at the beginning of the string
+        1 = Returns a match where the specified characters are at the beginning or at the end of a word
+        (the "r" in the beginning is making sure that the string is being treated as a "raw string")
+        2 = Returns a match where the specified characters are present, but NOT at the beginning (or at the end) of a word
+        (the "r" in the beginning is making sure that the string is being treated as a "raw string")
+        3 = Returns a match where the string contains digits (numbers from 0-9)
+        4 = Returns a match where the string DOES NOT contain digits
+        5 = Returns a match where the string contains a white space character
+        6 = Returns a match where the string DOES NOT contain a white space character
+        7 = Returns a match where the string contains any word characters (characters from a to Z, digits from 0-9, and the underscore _ character)
+        8 = Returns a match where the string DOES NOT contain any word characters
+        9 = Returns a match if the specified characters are at the end of the string
+        """
+        sequence_characters = ["A","b","B","d","D","s","S","w","W","Z"]
+        if n > len(sequence_characters):
+            raise Warning("out of index with the special sequences")
+        return self.special_sequence(sequence_characters[n])
